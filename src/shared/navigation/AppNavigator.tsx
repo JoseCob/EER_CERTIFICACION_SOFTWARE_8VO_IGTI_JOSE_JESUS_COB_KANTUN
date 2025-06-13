3d6563375012be7abda92a74e2716112e2427fa7
JoseCob <josecobkantun123@gmail.com>
2025-06-12 21:45:01 -0600

    Se agrego la page de Dashboard
    Se esta preparando la feature del dashborad de la app

--- src/shared/navigation/AppNavigator.tsx
+++ src/shared/navigation/AppNavigator.tsx
@@ -1,12 +1,14 @@
 //Navegación: react Navigator con bottom tabs
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from '../../features/home/presentation/ui/pages/HomePage';
+import DashPage from '../../features/dashboard/presentation/ui/page/DashPage';
 
 const Tabs = createBottomTabNavigator();
 
 const AppNavigator = () => (
     <Tabs.Navigator>
         <Tabs.Screen name="Inicio" component={HomePage}/>
+        <Tabs.Screen name="Resultados" component={DashPage}/>
     </Tabs.Navigator>
 );
 
